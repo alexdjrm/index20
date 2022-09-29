908fbf20f29d239c2f53cab2cf838918962adbe8
afenicio@natsystem.fr <alexdjrm@gmail.com>
2022-09-29 18:03:07 +0200

    Rotating Compass on 11.25 intervals

--- app/src/main/java/com/afeni/index20/MainActivity.java
+++ app/src/main/java/com/afeni/index20/MainActivity.java
@@ -6,6 +6,7 @@ import android.hardware.SensorEventListener2;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 import com.afeni.index20.controllers.WindManager;
 import com.afeni.index20.models.Wind;
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     TextView logX;
     TextView logY;
     TextView logZ;
+    ImageView imageViewProtractorPointer;
     float[] mGravity;
     float[] mGeomagnetic;
     float azimut;
@@ -31,21 +33,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onCreate(savedInstanceState);
         this.index20 = WindManager.getWinds(getResources().getStringArray(R.array.index20));
         setContentView(R.layout.activity_main);
-
         this.logMAG = findViewById(R.id.logMAG);
         this.logMAG.setText("MAG");
-
         this.logORI = findViewById(R.id.logORI);
         this.logORI.setText("ORI");
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-
-
         this.logX = findViewById(R.id.logX);
         this.logX.setText(index20.get(0).getName());
         this.logY = findViewById(R.id.logY);
         this.logY.setText(index20.get(0).getCardinal());
         this.logZ = findViewById(R.id.logZ);
         this.logZ.setText(String.valueOf(index20.get(0).getFloat_value()));
+        imageViewProtractorPointer = findViewById(R.id.compass);
     }
 
     @Override
@@ -62,7 +61,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     protected void onPause() {
         super.onPause();
-
         // to stop the listener and save battery
         mSensorManager.unregisterListener(this);
     }
@@ -86,16 +84,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             float I[] = new float[9];
 
             if (SensorManager.getRotationMatrix(R, I, mGravity, mGeomagnetic)) {
-
                 // orientation contains azimut, pitch and roll
                 float orientation[] = new float[3];
                 SensorManager.getOrientation(R, orientation);
-
                 azimut = orientation[0];
             }
         }
         float rotation = Math.round(-azimut * 360 / (2 * 3.14159f));
         this.logMAG.setText(String.valueOf(rotation));
+        imageViewProtractorPointer.setRotation(roundRotation(rotation));
     }
 
     @Override
@@ -106,5 +103,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
         super.onPointerCaptureChanged(hasCapture);
+    }
+
+    private float roundRotation(float rotation){
+        Double res = Math.floor(rotation/11.25)*11.25;
+        return res.floatValue();
     }
 }
